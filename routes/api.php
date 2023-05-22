ecf99724bf6699069c7251b3914a0da7f60d452c
brayan <rfreyrebrayaned@crece.uss.edu.pe>
2023-05-22 14:28:19 -0500

    apis category

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SubCategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::resource('empleados',EmployeeController::class)->parameters(['empleados'=>'employee'])->names('employee');
+Route::resource('productos',ProductController::class)->parameters(['productos'=>'products'])->names('products');
+Route::resource('categorias',CategoryController::class)->parameters(['categorias'=>'categories'])->names('categories');
+Route::resource('subcategorias',SubCategoryController::class)->parameters(['subcategorias'=>'sub_categories'])->names('sub_categories');
